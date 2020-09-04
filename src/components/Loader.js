bc6a539d8e6aae039f3d93a2687bac869b85b749
Marius Kroh <mail@mariuskroh.de>
2020-09-04 11:49:03 +0200

    :zap: Cleanup Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,32 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Dot from "./Dot";
 import styles from "./LoaderStyles";
 
-const Loader = ({ loaded }) => {
+const Loader = () => {
   const classes = styles();
 
   return (
     <div className={classes.loader}>
-      <Dot delay="0s" color="green" />
-      <Dot delay="0.1s" color="red" />
-      <Dot delay="0.2s" color="blue" />
+      <Dot delay="0s" color="black" />
+      <Dot delay="0.1s" color="black" />
+      <Dot delay="0.2s" color="black" />
     </div>
   );
-  //   useEffect(() => {
-  //     if (loaded) {
-  //       console.log(loaded);
-  //     }
-  //   });
-  //   useEffect(() => {
-  //     if (dots.length <= 3) {
-  //       setTimeout(() => {
-  //         setDots((state) => state + ".");
-  //       }, 300);
-  //     } else {
-  //       setDots("");
-  //     }
-  //   }, [dots.length]);
-  //   return dots;
 };
 
 export default Loader;
