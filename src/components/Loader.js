c8cc40bbc8446c5ed44f9dacda45d99a3daa64be
Marius Kroh <mail@mariuskroh.de>
2020-09-05 19:51:33 +0200

    :zap: Add redirect when loaded

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,17 +1,22 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import Dot from "./Dot";
 import styles from "./LoaderStyles";
 
-const Loader = () => {
+const Loader = ({ loading }) => {
   const classes = styles();
 
-  return (
-    <div className={classes.loader}>
-      <Dot delay="0s" color="black" />
-      <Dot delay="0.1s" color="black" />
-      <Dot delay="0.2s" color="black" />
-    </div>
-  );
+  if (loading) {
+    return (
+      <div className={classes.loader}>
+        <Dot delay="0s" color="black" />
+        <Dot delay="0.1s" color="black" />
+        <Dot delay="0.2s" color="black" />
+      </div>
+    );
+  } else {
+    return <Redirect to="/search" />;
+  }
 };
 
 export default Loader;
