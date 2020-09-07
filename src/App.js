405e8afd9ce61a7f14fe1e2cb6e3b89eac7a235e
Marius Kroh <mail@mariuskroh.de>
2020-09-07 11:54:19 +0200

    :zap: Cleanup imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import CryptoStatusWidget from "./components/CryptoStatusWidget";
 import "./App.css";
 
